d9cdd21c9c56c979b1a372aef4a250b6b3fb76a7
Morgan Trotter <mhtrotte@ucsc.edu>
2021-01-15 23:40:00 -0800

    Added numbers to text on barchart

--- src/components/BarChart.js
+++ src/components/BarChart.js
@@ -34,6 +34,9 @@ class BarChart extends Component {
     }
 
     const dataArray = [fieldData, throwData, hitData];
+    const textArray = ["Soft Hands: " + fieldData[0].toString(), "Footwork: " + fieldData[1].toString(), "Backhand: " + fieldData[2].toString(),
+        "Arm Action: " + throwData[0].toString(), "Footwork: " + throwData[1].toString(), "Balance: " + throwData[2].toString(),
+        "Contact: " + hitData[0].toString(), "Power: " + hitData[1].toString(),  "Balance: " + hitData[2].toString()];
     var arrayIndex = 1;
     const svg = d3.select("#fieldingchart")
 	    .append("svg")
@@ -72,16 +75,70 @@ class BarChart extends Component {
       .attr("fill", "white")
       .attr("id", "whitebar");
 
+    svg.selectAll("g")
+      .data(fieldData)
+      .enter()
+      .append("rect")
+      .attr("x", 10)
+      .attr("y", (d, i) => i * 70 + 15)
+      .attr("width", 120)
+      .attr("height", 35)
+      .attr("id", "graytextbar")
+      .style("fill", "gray")
+      .style("opacity", 0.7);
+
     svg.append("text")
 	   .attr("x", 20)
 	   .attr("y", 40)
+     .attr("id", "viztextone")
 	   .style("fill", "white")
-	   .text("Hello");
+     .style("font-family", "'Oswald', sans-serif")
+	   .text("Soft Hands: " + fieldData[0]);
+
+    svg.append("text")
+     .attr("x", 20)
+     .attr("y", 110)
+     .attr("id", "viztexttwo")
+     .style("fill", "white")
+     .style("font-family", "'Oswald', sans-serif")
+     .text("Footwork: " + fieldData[1]);
+
+    svg.append("text")
+     .attr("x", 20)
+     .attr("y", 180)
+     .attr("id", "viztextthree")
+     .style("fill", "white")
+     .style("font-family", "'Oswald', sans-serif")
+     .text("Backhand: " + fieldData[2]);
 
     svg.on("click", function() {
     	if (arrayIndex === 3) {
     		arrayIndex = 0;
     	}
+      svg.select("#viztextone")
+        .transition()
+        .duration(1000)
+        .attr("x", 20)
+        .attr("y", 40)
+        .style("fill", "white")
+        .text(textArray[3 * arrayIndex]);
+
+      svg.select("#viztexttwo")
+        .transition()
+        .duration(1000)
+        .attr("x", 20)
+        .attr("y", 110)
+        .style("fill", "white")
+        .text(textArray[3 * arrayIndex + 1]);
+
+      svg.select("#viztextthree")
+        .transition()
+        .duration(1000)
+        .attr("x", 20)
+        .attr("y", 180)
+        .style("fill", "white")
+        .text(textArray[3 * arrayIndex + 2]);
+
     	svg.selectAll("#datarect")
     		.data(dataArray[arrayIndex])
     		.transition()
